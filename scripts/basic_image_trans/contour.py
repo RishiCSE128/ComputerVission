d8e6b8e83227ef10ebb98449215a3f57f8874e94
RishiCSE128 <saptarshi.ghosh.cs2@gmail.com>
2021-12-02 23:46:35 +0000

    modefies cotour

--- scripts/basic_image_trans/contour.py
+++ scripts/basic_image_trans/contour.py
@@ -8,6 +8,61 @@ Process images with
 
 import cv2 as cv
 
+
+def image_proc(img):
+    #gray scaling 
+    frame_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    #Thresholding
+    thresh = 125
+    ret, frame_thresold = cv.threshold(frame_gray, 
+                                thresh=thresh, 
+                                maxval=255, 
+                                type=cv.THRESH_BINARY)
+   
+
+    #Blurring (reduces contour size)
+    conv = 5      # has to be an odd number
+    frame_blur = cv.GaussianBlur(frame_gray, 
+                                ksize=(conv, conv),  # convolution size (odd number)
+                                sigmaX=0,
+                                borderType=cv.BORDER_DEFAULT)
+    
+
+    #edge detection 
+    frame_edge = cv.Canny(frame_blur, threshold1=100, threshold2=200)
+    
+    #contour detection
+    # cont = list of contours 
+    # hier = shape hierarchy 
+    cont, hier = cv.findContours(frame_edge,          # edge detected source
+                                mode=cv.RETR_LIST,    # mode of contour finding, RETR_LIST = all cont found
+                                method=cv.CHAIN_APPROX_SIMPLE) # approximation method [NONE = all, SIMPLE = compressed into one]
+    
+    frame_cont = cv.drawContours(img, cont, -1, (0,0,255),thickness=2)
+
+    cv.putText(frame_cont,
+                text=f'Contour found = {len(cont)}',
+                org=(10,50), 
+                fontFace=cv.FONT_HERSHEY_PLAIN,
+                fontScale=2,
+                color=(0,0,255),
+                thickness=2
+            )
+
+    ret_dict = {
+        'gray'      : {'frame': frame_gray},
+        'threshold' : {'frame': frame_thresold, 
+                        'thresh':thresh},
+        'blur'      : {'frame': frame_blur, 
+                       'conv': conv},
+        'edge'      : {'frame' : frame_edge, 
+                       'thresh' : [100,200]},
+        'contour'   : {'frame': frame_cont, 
+                       'count' : len(cont)}
+    }
+    return ret_dict
+    
 def read_feed(cam, res):
     capture = cv.VideoCapture(cam)
 
@@ -19,48 +74,21 @@ def read_feed(cam, res):
         isTrue, frame = capture.read()
         cv.imshow("live feed", frame)
 
-        #gray scaling 
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        #cv.imshow("live feed (Gray)", frame_gray)
+        proc_dict = image_proc(img=frame)
 
-        #Thresholding
-        thresh = 125
-        ret, frame_thresold = cv.threshold(frame_gray, 
-                                    thresh=thresh, 
-                                    maxval=255, 
-                                    type=cv.THRESH_BINARY)
-        #cv.imshow(f"live feed (thresholded {thresh})", frame_thresold)
+        cv.imshow("live feed (gray)", proc_dict['gray']['frame'])
 
-        #Blurring (reduces contour size)
-        conv = 5      # has to be an odd number
-        frame_blur = cv.GaussianBlur(frame_gray, 
-                                    ksize=(conv, conv),  # convolution size (odd number)
-                                    sigmaX=0,
-                                    borderType=cv.BORDER_DEFAULT)
-        #cv.imshow("live feed (Blur)", frame_blur)
+        cv.imshow(f"live feed (thresholded {proc_dict['threshold']['thresh']})", 
+                   proc_dict['threshold']['frame']
+        )
 
-        #edge detection 
-        frame_edge = cv.Canny(frame_blur, threshold1=100, threshold2=200)
-        cv.imshow("live feed (Edge)", frame_edge)
+        cv.imshow(f"live feed (Blur with conv = {proc_dict['blur']['conv']})",
+                  proc_dict['blur']['frame']
+        )
 
-        #contour detection
-        # cont = list of contours 
-        # hier = shape hierarchy 
-        cont, hier = cv.findContours(frame_edge,          # edge detected source
-                                    mode=cv.RETR_LIST,    # mode of contour finding, RETR_LIST = all cont found
-                                    method=cv.CHAIN_APPROX_SIMPLE) # approximation method [NONE = all, SIMPLE = compressed into one]
-        frame_cont = cv.drawContours(frame, cont, -1, (0,0,255),thickness=2)
-        print(f'Contour found = {len(cont)}')
-        cv.putText(frame_cont,
-                    text=f'Contour found = {len(cont)}',
-                    org=(10,50), 
-                    fontFace=cv.FONT_HERSHEY_PLAIN,
-                    fontScale=2,
-                    color=(0,0,255),
-                    thickness=2
-                )
-        cv.imshow("live feed (contours)", frame_cont)
+        cv.imshow("live feed (Edge)",proc_dict['edge']['frame'])
 
+        cv.imshow("live feed (contours)",proc_dict['contour']['frame'])
 
         if cv.waitKey(20) & 0xFF == ord('d'):
                 break
